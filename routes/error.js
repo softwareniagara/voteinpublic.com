b9321687bf7f95da257835e35088a6b886796c5f
Michael Mottola <mikemottola@gmail.com>
2012-11-11 16:23:38 -0500

    Added view for no location.

--- routes/error.js
+++ routes/error.js
@@ -14,3 +14,11 @@ exports.get404 = function(req, res){
 exports.get500 = function(req, res){
   res.status(500).render('./../views/errors/404', { title: '500 Application Error' });
 };
+
+/*
+ * GET errors/no_location
+ */
+
+exports.no_location = function(req, res){
+  res.render('./../views/errors/no_location', { title: 'You must supply your location to vote' });
+};
