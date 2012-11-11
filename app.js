8d9f7a81df3e311d5df76861ca5194edcb859f48
Nickolas Kenyeres <nkenyeres@gmail.com>
2012-11-10 20:03:22 -0500

    Added question router to the app.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , question = require('./routes/question')
   , http = require('http')
   , path = require('path')
   , mongoose = require('mongoose')
@@ -61,6 +62,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/questions', question.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
