ddb851c0c429fa94e975171beb5a869103346f4a
Dennis Ideler <ideler.dennis@gmail.com>
2012-11-10 19:07:09 -0500

    Use different port -- had issues running it on 3000

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function compile(str, path) {
 }
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3001);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(stylus.middleware({
